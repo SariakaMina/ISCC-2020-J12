ff51eb89a6002ab4c5253c6578ff92f4e5f0eedf
SariakaMina <67281772+SariakaMina@users.noreply.github.com>
2020-07-11 11:08:11 +0200

    Add files via upload

---
+++ J12/connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="vitrine.css"/>
+        <meta charset="utf-8">
+        <title>mini-site-routing</title>  
+    </head>
+
+<body>
+
+<form action="securite.php" method="post">
+    <input type="text" placeholder="Login" name="login">
+    <input type="password" placeholder="Password" name="password">
+    <input type="submit" value="Connexion" name="submit">
+</form>
